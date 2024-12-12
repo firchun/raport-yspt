6e948ab17026ca5d7884d2e294ecc237e0b9f193
MacBook Air <firchun025@gmail.com>
2024-12-12 16:26:37 +0900

    update edit komentar

--- resources/views/admin/penilaian/print.blade.php
+++ resources/views/admin/penilaian/print.blade.php
@@ -55,111 +55,111 @@
     <!-- Informasi Santri -->
 
 
-    @if ($data->count() > 0)
-        @foreach ($data->groupBy('id_santri') as $idSantri => $santriData)
-            <table class="info-table">
-                <tr>
-                    <td> <img src="{{ public_path('/img') }}/logo-pesantren.png" alt="Logo Kiri" class="logo"
-                            style="width: 80px;"></td>
-                    <td>
-                        <h1>Laporan Kepesantrenan</h1>
-                    </td>
-                    <td> <img src="{{ public_path('/img') }}/logo-yayasan.png" alt="Logo Kanan" class="logo"
-                            style="width: 100px;"></td>
-                </tr>
-            </table>
-            @php
-                $santri = $santriData->first()->santri ?? null;
-                $tahunAjaran = $santriData->first()->tahun_ajaran ?? null;
-            @endphp
+    {{-- @if ($data->count() > 0) --}}
+    @foreach ($data->groupBy('id_santri') as $idSantri => $santriData)
+        <table class="info-table">
+            <tr>
+                <td> <img src="{{ public_path('/img') }}/logo-pesantren.png" alt="Logo Kiri" class="logo"
+                        style="width: 80px;"></td>
+                <td>
+                    <h1>Laporan Kepesantrenan</h1>
+                </td>
+                <td> <img src="{{ public_path('/img') }}/logo-yayasan.png" alt="Logo Kanan" class="logo"
+                        style="width: 100px;"></td>
+            </tr>
+        </table>
+        @php
+            $santri = $santriData->first()->santri ?? null;
+            $tahunAjaran = $santriData->first()->tahun_ajaran ?? null;
+        @endphp
 
-            <!-- Informasi Santri -->
+        <!-- Informasi Santri -->
 
-            <table class="info-table">
-                <tr>
-                    <td><strong>Nama</strong></td>
-                    <td>: {{ $santri->nama ?? '-' }}</td>
-                </tr>
-                <tr>
-                    <td><strong>Kelas/Semester/TA</strong></td>
-                    <td>:
-                        {{ $santri->kelas ?? '-' }}/{{ $tahunAjaran->semester ?? '-' }}/{{ $tahunAjaran->tahun ?? '-' }}
-                    </td>
-                </tr>
-                <tr>
-                    <td><strong>Kamar</strong></td>
-                    <td>: {{ $santri->kamar ?? '-' }}</td>
-                </tr>
-            </table>
+        <table class="info-table">
+            <tr>
+                <td><strong>Nama</strong></td>
+                <td>: {{ $santri->nama ?? '-' }}</td>
+            </tr>
+            <tr>
+                <td><strong>Kelas/Semester/TA</strong></td>
+                <td>:
+                    {{ $santri->kelas ?? '-' }}/{{ $tahunAjaran->semester ?? '-' }}/{{ $tahunAjaran->tahun ?? '-' }}
+                </td>
+            </tr>
+            <tr>
+                <td><strong>Kamar</strong></td>
+                <td>: {{ $santri->kamar ?? '-' }}</td>
+            </tr>
+        </table>
 
-            <!-- Tabel Penilaian -->
-            @foreach ($santriData->groupBy('id_kategori') as $kategoriId => $kategoriData)
-                <table>
-                    <thead>
-                        <tr class="category">
-                            <th colspan="3">{{ $kategoriData->first()->kategori->kategori ?? '-' }}</th>
-                        </tr>
+        <!-- Tabel Penilaian -->
+        @foreach ($santriData->groupBy('id_kategori') as $kategoriId => $kategoriData)
+            <table>
+                <thead>
+                    <tr class="category">
+                        <th colspan="3">{{ $kategoriData->first()->kategori->kategori ?? '-' }}</th>
+                    </tr>
+                    <tr>
+                        <th>No.</th>
+                        <th>Aspel Penilaian</th>
+                        <th>Nilai</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($kategoriData as $index => $penilaian)
                         <tr>
-                            <th>No.</th>
-                            <th>Aspel Penilaian</th>
-                            <th>Nilai</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($kategoriData as $index => $penilaian)
-                            <tr>
-                                <td>{{ $index + 1 }}</td>
-                                <td>{{ $penilaian->point->point ?? '-' }}</td>
-                                <td>
-                                    @if ($penilaian->nilai == 1)
-                                        Belum Tampak
-                                    @elseif($penilaian->nilai == 2)
-                                        Berkembang
-                                    @elseif($penilaian->nilai == 3)
-                                        Mandiri
-                                    @else
-                                        -
-                                    @endif
-                                </td>
-                            </tr>
-                        @endforeach
-                        <tr>
-                            <td colspan="3">
-                                Komentar Tambahan : "
-                                <i>{{ App\Models\KomentarPenilaian::where('id_kategori', $kategoriId)->where('id_santri', $penilaian->id_santri)->where('id_tahun_ajaran', $penilaian->id_tahun_ajaran)->first()->komentar ?? '-' }}</i>
-                                "
+                            <td>{{ $index + 1 }}</td>
+                            <td>{{ $penilaian->point->point ?? '-' }}</td>
+                            <td>
+                                @if ($penilaian->nilai == 1)
+                                    Belum Tampak
+                                @elseif($penilaian->nilai == 2)
+                                    Berkembang
+                                @elseif($penilaian->nilai == 3)
+                                    Mandiri
+                                @else
+                                    -
+                                @endif
                             </td>
                         </tr>
-                    </tbody>
-                </table>
-            @endforeach
-            <!-- Tanda Tangan -->
-            <div style="margin-top: 60px; clear: both;">
-                <div style="width: 50%; float: left; text-align: center;">
-                    <p></p>
-                    <p style="margin-top: 80px;">
-                        <b>
-                            @php
-                                $pengasuh =
-                                    App\Models\PengasuhPenilaian::where('id_santri', $santri->id ?? 0)
-                                        ->where('id_tahun_ajaran', $tahunAjaran->id ?? 0)
-                                        ->first()->pengasuh->nama ?? '-';
-                            @endphp
-                            {{ $pengasuh }}
-                        </b><br>(Biro Pengasuhan)
-                    </p>
-                </div>
-                <div style="width: 50%; float: right; text-align: center;">
-                    <p>Merauke, {{ date('d F Y') }}<br>Mengetahui</p>
-                    <p style="margin-top: 60px;"><b>Buya M. Dic Hidayat Ratuloly, S.PdI, MPS.</b><br>(Pimpinan Pondok
-                        Pesantren)</p>
-                </div>
-            </div>
-            <div class="page-break"></div>
+                    @endforeach
+                    <tr>
+                        <td colspan="3">
+                            Komentar Tambahan : "
+                            <i>{{ App\Models\KomentarPenilaian::where('id_kategori', $kategoriId)->where('id_santri', $penilaian->id_santri)->where('id_tahun_ajaran', $penilaian->id_tahun_ajaran)->first()->komentar ?? '-' }}</i>
+                            "
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         @endforeach
-    @else
+        <!-- Tanda Tangan -->
+        <div style="margin-top: 60px; clear: both;">
+            <div style="width: 50%; float: left; text-align: center;">
+                <p></p>
+                <p style="margin-top: 80px;">
+                    <b>
+                        @php
+                            $pengasuh =
+                                App\Models\PengasuhPenilaian::where('id_santri', $santri->id ?? 0)
+                                    ->where('id_tahun_ajaran', $tahunAjaran->id ?? 0)
+                                    ->first()->pengasuh->nama ?? '-';
+                        @endphp
+                        {{ $pengasuh }}
+                    </b><br>(Biro Pengasuhan)
+                </p>
+            </div>
+            <div style="width: 50%; float: right; text-align: center;">
+                <p>Merauke, {{ date('d F Y') }}<br>Mengetahui</p>
+                <p style="margin-top: 60px;"><b>Buya M. Dic Hidayat Ratuloly, S.PdI, MPS.</b><br>(Pimpinan Pondok
+                    Pesantren)</p>
+            </div>
+        </div>
+        <div class="page-break"></div>
+    @endforeach
+    {{-- @else
         <p>Data tidak tersedia untuk Santri dan Tahun Ajaran yang dipilih.</p>
-    @endif
+    @endif --}}
 
 
 </body>
